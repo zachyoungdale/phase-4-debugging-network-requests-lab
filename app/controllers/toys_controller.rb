3517d0621ce928d5811bb2e134403ef70f996a8e
Zachary Youngdale <zyoungdale@gmail.com>
2022-11-02 16:10:01 -0400

    commit tree 6ae4c0d07f5f61d0b0545ecfc60b8faa78132661

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,16 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toys.create!(toy_params)
     render json: toy, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid 
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy.update!(toy_params)
+    render json: toy, status: :ok
   end
 
   def destroy
--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,2 +1,4 @@
 class Toy < ApplicationRecord
+    validates :name, presence: true
+    validates :image, presence: true
 end
